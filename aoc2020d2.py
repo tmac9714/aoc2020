28317a56529e8fb950f32335b9f3200517ff51ef
JerryYu <tmac9714@gmail.com>
2020-12-02 17:20:46 +0800

    first commit

--- aoc2020d2.py
+++ aoc2020d2.py
@@ -7,22 +7,27 @@ Created on Tue Dec  1 09:36:13 2020
 
 import os
 os.getcwd()
+os.chdir("../aoc2020")
 
-file = "inputd1.txt"
-list = []
+file = "inputd2.txt"
+
+counter1 = 0
+counter2 = 0
 with open(file) as fp:
     for line in fp:
-        list.append(int(line.strip()))
+        nm,word,strs=line.split()
+        n,m=nm.split('-')
+        n,m=int(n),int(m)
+        word=word.strip(':')
+        if strs.count(word)<=m and strs.count(word)>=n:
+            counter1 += 1
+        if strs[n-1]==word and strs[m-1]!=word:
+            counter2 += 1
+        if strs[n-1]!=word and strs[m-1]==word:
+            counter2 += 1
+print(counter1)             
+print(counter2)
 
-for i in list:
-    for j in list:
-        if i+j==2020:
-            print(i*j)
-            break
-    
-for i in list:
-    for j in list:
-        for k in list:
-            if i+j+k==2020:
-                print(i*j*k)
-                break      
+
+
+            
