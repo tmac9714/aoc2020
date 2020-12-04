c0c52c67f6cd1dd8831951120ae74fc05ca4813a
JerryYu <tmac9714@gmail.com>
2020-12-04 17:53:19 +0800

    first commit

---
+++ aoc2020d3.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  1 09:36:13 2020
+
+@author: user
+"""
+import os
+os.getcwd()
+os.chdir("./aoc2020")
+
+file = "inputd3.txt"
+
+x=0
+counter=0
+with open(file) as fp:
+    for line in fp:
+        line=line.strip()
+        #print(line[x%len(line)])
+        if line[x%len(line)]=='#':           
+            counter +=1
+        x +=3     
+print(counter)      
+
+v=0
+w=0
+x=0
+y=0
+z=0
+a=0
+b=0
+c=0
+d=0
+e=0
+f=0
+linenum=1
+with open(file) as fp:
+    
+    for line in fp:
+        line=line.strip()
+        if line[w%len(line)]=='#':           
+            a +=1
+        w +=1
+        if line[x%len(line)]=='#':           
+            b +=1
+        x +=3
+        if line[y%len(line)]=='#':           
+            c +=1
+        y +=5
+        if line[z%len(line)]=='#':           
+            d +=1
+        z +=7
+        if linenum%2==1 :            
+            if line[v%len(line)]=='#':                
+                e +=1
+            v +=1
+        linenum += 1
+print(a*b*c*d*e)
+
