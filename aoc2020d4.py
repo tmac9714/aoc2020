8c87b49ee56657541a71e10a24a790bca0380ba8
JerryYu <tmac9714@gmail.com>
2020-12-10 15:55:08 +0800

    first commit

--- aoc2020d4.py
+++ aoc2020d4.py
@@ -6,3 +6,12 @@ Created on Mon Dec  7 09:36:56 2020
 """
 
 
+import os
+os.getcwd()
+os.chdir("./aoc2020")
+
+file="inputd4.txt"
+f=open(file).read().split("\n\n")
+for line in f:
+    passport =line.split()
+    print(passport)
